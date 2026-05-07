13b9043e818dd0b691cc81beb64dd7b8efd17ecf
Matt Muszynski <matt.muszynski@jpl.nasa.gov>
2026-05-07 16:00:57 -0700

    makes an attempt

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ import os
 import sys
 import importlib.metadata
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
